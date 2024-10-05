119411a0107ad90b9a7c64d87eda67ab7b58acc6
Sharathchandra <sharathchandra9204@gmail.com>
2024-10-05 16:22:36 +0530

    Task 4 submission

--- filterNumber.js
+++ filterNumber.js
@@ -1,1 +1,15 @@
+//const num=process.argv[2];
+//const evenodd=process.argv[3];
+function filterNumbers(arr, criterion) {
+    switch(criterion) {
+        case 'even':
+            return arr.filter(num => num % 2 === 0);
+        case 'odd':
+            return arr.filter(num => num % 2 !==0);
+        default:
+            return arr;
+    }
+}
 
+console.log(filterNumbers([1,2,3,4,4,5,6],'even'));
+console.log(filterNumbers([1,2,3,4,4,5,6],'odd'));
